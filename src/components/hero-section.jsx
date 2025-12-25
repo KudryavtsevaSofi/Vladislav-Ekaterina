35230ed0ad207f7a0ebca7bfaec25617d67780e9
Кудрявцева Соня дом <kudryavtseva.sofi@yandex.ru>
2025-12-25 11:41:20 +0400

    Реализовала десктопную версию, импортировала шрифты и картинки

---
+++ src/components/hero-section.jsx
@@ -0,0 +1,12 @@
+import Photo from '../assets/images/photo.svg';
+import Flowers from '../assets/images/flowers.svg';
+
+const HeroSection = () => {
+  return (
+    <div className='page-container'>
+      <p>Это мобильная версия</p>
+    </div>
+  );
+};
+
+export default HeroSection;
---
+++ src/pages/main.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import styles from './main.module.scss';
+import StickSmall from '../assets/images/stick-small.svg';
+import HeroSection from '../components/hero-section';
+
+const MainPage = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 800px)');
+    const handleMediaChange = (e) => setIsMobile(e.matches);
+    
+    setIsMobile(mediaQuery.matches);
+    mediaQuery.addEventListener('change', handleMediaChange);
+    
+    return () => mediaQuery.removeEventListener('change', handleMediaChange);
+  }, []);
+
+  return (
+    <div className={styles.page}>
+      {isMobile ? (
+        <div className={styles.mobile}>
+          <HeroSection />
+        </div>
+      ) : (
+        <div className={styles.desktop}>
+          <div className={styles.desktop__content}>
+            <p className={styles.desktop__text}>Пожалуйста, откройте сайт через <br/>мобильное устройство</p>
+            <img src={StickSmall} className={styles.desktop__stick} alt='Веточка' />
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default MainPage;
